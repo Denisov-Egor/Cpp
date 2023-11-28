26da66ecc5366b404ccd1f0edd42c1c84fd33dd3
Denisov-Egor <programming2028@gmail.com>
2023-11-28 23:42:18 +0300

    Class

---
+++ Lessons/Stepik/Class3.cpp
@@ -0,0 +1,15 @@
+// Дано натуральное число, выведите его последнюю цифру.
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int a, b;
+
+    cout << "Введите натуральное число: ";
+    cin >> a;
+
+    b = a % 10;
+    cout << x;
+}
