1c48cde3ec3cece2a26181220e94afc3c6ac4058
Denisov-Egor <programming2028@gmail.com>
2023-11-29 23:14:59 +0300

    Task

--- Lessons/Stepik/Task5.cpp
+++ Lessons/Stepik/Task5.cpp
@@ -3,7 +3,12 @@
 #include <iostream>
 using namespace std;
 
-int main()
+int main() 
 {
-    
+    int a;
+
+    cout << "Введите целое трехзначное число: ";
+    cin >> a;
+
+    cout << a % 10 + a % 100 / 10 + a / 100 ;
 }
