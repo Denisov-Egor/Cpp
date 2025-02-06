898d17965a2f23e1dc9b2973ee1142dcf607b613
Denisov-Egor <129548353+Denisov-Egor@users.noreply.github.com>
2025-02-06 19:39:50 +0300

    9

--- Study/laboratory work 2 option 6/3.cpp
+++ Study/laboratory work 2 option 6/3.cpp
@@ -7,5 +7,36 @@ using namespace std;
 
 int main()
 {
+    int day;
 
+    cout << "Введите день недели: ";
+    cin >> day;
+
+    switch (day)
+    {
+    case 1:
+        cout << "Понедельник" << endl;
+        break;
+    case 2:
+        cout << "Вторник" << endl;
+        break;
+    case 3:
+        cout << "Среда" << endl;
+        break;
+    case 4:
+        cout << "Четверг" << endl;
+        break;
+    case 5:
+        cout << "Пятница" << endl;
+        break;
+    case 6:
+        cout << "Суббота" << endl;
+        break;
+    case 7:
+        cout << "Воскресенье" << endl;
+        break;
+    default:
+        cout << "Неверный день недели" << endl;
+        break;
+    }
 }
