2faa0117ed2658034377cb7ac3a3e44325fe4bfd
Denisov-Egor <129548353+Denisov-Egor@users.noreply.github.com>
2025-12-09 09:25:09 +0300

    50

--- PGU/Denisov Egor/laba8/1.cpp
+++ PGU/Denisov Egor/laba8/1.cpp
@@ -2,40 +2,42 @@
 
 using namespace std;
 
-
 int main() 
 {
-    const int c = 100;
-    int n, a[c];
-    int max = a[0];
+    const int c = 100; 
+    int n, mas[c], max;
 
     do {
-        cout << "Введите n: ";
+        cout << "Введите количество элементов n (0 < n <= " << c << "): ";
         cin >> n;
     } while (n <= 0 || n > c);
 
     cout << "Введите элементы массива:\n";
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++) 
     {
-        cin >> a[i];
+        cin >> mas[i];
     }
 
-    for (int i = 1; i < n; i++)
+    max = mas[0];
+    for (int i = 1; i < n; i++) 
     {
-        if (a[i] > max) 
-        {   
-            max = a[i];
+        if (mas[i] > max) 
+        {
+            max = mas[i];
         }
     }
 
-    for (int i = 4; i < n; i += 5)
+    for (int i = 0; i < n; i++) 
     {
-        a[i] = max;
+        if ((i + 1) % 5 == 0) {
+            mas[i] = max;
+        }
     }
 
-    cout << "Полученный массив:\n";
-    for (int i = 0; i < n; i++)
+    cout << "Изменённый массив:\n";
+    for (int i = 0; i < n; i++) 
     {
-        cout << a[i] << " ";
+        cout << mas[i] << " ";
     }
+    cout << endl;
 }
