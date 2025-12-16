b97216e82fd10f2a670f37fef9e79b0c3fb9daa8
Denisov-Egor <129548353+Denisov-Egor@users.noreply.github.com>
2025-12-16 18:57:12 +0300

    59

---
+++ PGU/Lisin Ilay/laba 7/1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    double x = 0.5;  // Пример значения
+    int n = 10;      // Количество членов
+    
+    double result = x;
+    double term = x;
+    double x2 = x * x;
+    int i = 1;
+    
+    while (i < n) {
+        term = term * x2 * (2*i-1)*(2*i-1) / (2*i*(2*i+1));
+        result += term;
+        i++;
+    }
+    
+    cout << "Приближенное: " << result << endl;
+    cout << "Точное: " << asin(x) << endl;
+    
+    return 0;
+}
